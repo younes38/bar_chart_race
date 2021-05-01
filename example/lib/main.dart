8532285f4d3b8b512944ed1fabbea453d632fa03
younes38 <hy_mimene@esi.dz>
2021-05-01 21:59:22 +0100

    updated example

---
+++ example/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:example/random_data_example.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // is not restarted.
+        primarySwatch: Colors.blue,
+
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RandomDataExample(),
+    );
+  }
+}
